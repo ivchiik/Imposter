e1cb07e278f8928b402224dd8eab91412288001d
ivchiik <ivasaata@gmail.com>
2024-03-10 00:23:40 +0400

    translated FAQ page

--- src/i18n/ka.ts
+++ src/i18n/ka.ts
@@ -7,5 +7,33 @@ export default {
     "CHOOSE THE GAME SETTINGS: A WORD PACK, PLAYING TIME, THE NUMBER OF PLAYERS AND IMPOSTERS. BY THE WAY, YOU CAN ALSO CHOOSE THE PLAYING LANGUAGE AND PRACTICE FOREIGN LANGUAGES WITH FRIENDS!":
       "აირჩიეთ თამაშის პარამეტრები: სიტყვების პაკეტი, თამაშის დრო, რაოდენობა მოთამაშეები და მატყუარები. სხვათა შორის, თქვენ ასევე შეგიძლიათ აირჩიოთ თამაშის ენა და ივარჯიშოთ უცხო ენებში მეგობრებთან ერთად!",
     "WHAT NEXT?": "ᲨᲔᲛᲓᲔᲒᲘ ᲠᲐ ᲐᲠᲘᲡ?",
+    "THERE ARE TWO TYPES OF ROLES: LOCALS AND IMPOSTERS. PASS THE PHONE AROUND SO EVERYONE CAN GET THEIR ROLE.":
+      "არსებობს როლების ორი ტიპი: ადგილობრივები და მატყუარა. გაცვალეთ ტელეფონი, რათა ყველამ შეძლოს თავისი როლის ნახვა.",
+    "CAN ANOTHER PLAYER SEE MY ROLE?":
+      "შეუძლია თუ არა სხვა მოთამაშეს ნახოს ჩემი როლი?",
+    "NO, THEY CAN'T. WHEN YOU TAP ON THE CARD, IT FLIPS OVER. ON THE OTHER SIDE THERE IS INFORMATION ABOUT WHETHER YOU ARE A IMPOSTER, AND IF YOU ARE NOT, THERE IS A LOCATION. AFTER YOU TAP ONCE AGAIN THE CARD WILL DISAPPEAR AND YOU WILL SEE THE COVER OF THE NEXT CARD. PASS THE PHONE TO THE NEXT PLAYER SO THEY CAN FLIP THE CARD OVER AND GET THEIR ROLE.":
+      "არა, მათ არ შეუძლიათ. ბარათზე შეხებისას ის გადატრიალდება. მეორე მხარეს არის ინფორმაცია იმის შესახებ, ხარ თუ არა მატყუარა, და თუ არ ხარ, არის ადგილმდებარეობა. ერთხელ შეხების შემდეგ ბარათი გაქრება და დაინახავთ შემდეგი ბარათის ყდას. გადააწოდეთ ტელეფონი შემდეგ მოთამაშეს, რათა მათ შეძლონ ბარათის გადატრიალება და მათი როლის ნახვა.",
+    "HOW DO WE PLAY?": "როგორ ვთამაშობთ?",
+    "AFTER EVERYONE HAS GOT THEIR ROLE, START PLAYING. ASK EACH OTHER QUESTIONS CONNECTED WITH THE LOCATION FROM THE LOCALS' CARDS. FOR EXAMPLE: «WHAT IS THE COLOR OF THIS PLACE?», «HOW OFTEN DO YOU GO THERE?», «IS THIS A NICE PLACE TO VISIT?» THE LOCALS SHOULD EXPOSE THE IMPOSTER — THE PERSON WHO DOESN'T UNDERSTAND WHAT LOCATION THE OTHERS ARE TALKING ABOUT. THE IMPOSTER SHOULD ACT AS IF THEY KNEW THE LOCATION AND AT THE SAME TIME THEY SHOULD TRY TO GUESS IT.":
+      "მას შემდეგ, რაც ყველამ თავისი როლი მიიღო, დაიწყეთ თამაში. დაუსვით ერთმანეთს ლოკაციასთან დაკავშირებული კითხვები ადგილობრივების ბარათებიდან. მაგალითად: 'რა არის ამ ადგილის ფერი?','რამდენად ხშირად მიდიხართ?' ეს კარგი ადგილია მოსანახულებლად?» ადგილობრივებმა უნდა გაამოიჭირონ მატყუარა - ადამიანი, რომელსაც არ ესმის, რა ლოკაციაზე საუბრობენ სხვები. მატყუარა ისე უნდა მოიქცეს, თითქოს იცის მდებარეობა და ამავე დროს უნდა შეეცადოს გამოიცნოს.",
+    "I HAVE A GUESS ABOUT WHO THE IMPOSTER IS. WHAT SHOULD I DO?":
+      "მე ვიცი თუ ვინ არის მატყუარა. ᲠᲐ ᲣᲜᲓᲐ ᲒᲐᲕᲐᲙᲔᲗᲝ?",
+    "SAY: «I KNOW WHO THE IMPOSTER IS.» THEN ON THE COUNT OF THREE ALL THE PLAYERS SHOULD POINT AT THE PERSON WHO IS THE IMPOSTER, IN THEIR VIEW. IF ALL THE PLAYERS HAVE CHOSEN THE SAME PERSON, THIS PERSON HAS TO REVEAL THEIR ROLE. IF THEY ARE A LOCAL, THE IMPOSTER WINS. IF THEY ARE THE IMPOSTER, THE LOCALS WIN. IF THE PLAYERS HAVE POINTED AT DIFFERENT PEOPLE, THE GAME CONTINUES.":
+      'ვთქვათ: "მე ვიცი, ვინ არის მატყუარა." მაშინ სამ თვლაზე ყველა მოთამაშემ უნდა მიუთითოს იმ ადამიანზე, რომელიც არის მატყუარა, მათი აზრით. თუ ყველა მოთამაშემ აირჩია ერთი და იგივე ადამიანი, ამ ადამიანმა უნდა გამოავლინოს თავისი როლი. თუ ის ადგილობრივია, მატყუარა იმარჯვებს. თუ ის არის მატყუარა, ადგილობრივები იმარჯვებენ. თუ მოთამაშეებმა მიუთითეს სხვადასხვა ადამიანზე, თამაში გრძელდება.',
+    "I AM THE IMPOSTER AND I THINK I KNOW WHAT LOCATION THE OTHERS ARE TALKING ABOUT. WHAT DO I DO?":
+      "მე ვარ მატყუარა და ვფიქრობ, რომ ვიცი, რა ლოკაციაზე საუბრობენ სხვები. ᲠᲐ ᲒᲐᲕᲐᲙᲔᲗᲝ?",
+    "JUST SAY: «I KNOW THE LOCATION» AND THEN NAME IT. IF YOU ARE RIGHT - YOU WIN. IF YOU ARE WRONG — THE LOCALS WIN.":
+      "უბრალოდ თქვი: 'მე ვიცი ადგილმდებარეობა' და შემდეგ დაასახელე იგი. თუ მართალი ხარ - შენ იმარჯვებ. თუ ცდებით - ადგილობრივები იმარჯვებენ.",
+    "WHY IS THERE A TIMER?": "რატომ არის ტაიმერი?",
+    "IF THE LOCALS FIND OUT WHO THE IMPOSTER IS BEFORE THE TIME IS UP, THEY WIN. IF THE IMPOSTER MANAGES TO STAY UNEXPOSED FOR THE WHOLE TIME — THEY WIN.":
+      "თუ ადგილობრივები დროის ამოწურვამდე გაიგებენ ვინ არის მატყუარა, ისინი იმარჯვებენ. თუ მატყუარა ახერხებს მთელი დროის განმავლობაში დაუფარავად დარჩენას - ის იმარჯვებს.",
+    "HOW DO WE PLAY WITH SEVERAL IMPOSTERS IN A GAME?":
+      "როგორ ვეთამაშოთ თამაშში რამდენიმე მატყუარას?",
+    "THE NUMBER OF IMPOSTERS MUST BE LESS THAN THE NUMBER OF LOCALS. IF THERE ARE MORE THAN ONE IMPOSTERS, EACH OF THEM PLAYS FOR THEMSELVES. TO WIN THE LOCALS HAVE TO EXPOSE ALL THE IMPOSTERS. IF THEY DON'T DISLOSE ALL THE IMPOSTERS, THE ONES WHO WERE NOT EXPOSED WIN. WHEN THE PLAYERS ARE POINTING AT A PERSON WHO IS LIKELY TO BE A IMPOSTERS: IF THEY ARE A LOCAL — THE IMPOSTERS WIN. IF THEY ARE A IMPOSTERS — THEY LEAVE THE GAME AND THE GAME CONTINUES UNTIL ALL THE IMPOSTERS ARE FOUND. WHEN ONE OF THE IMPOSTERS NAMES THE LOCATION: IF THEY ARE RIGHT — THE GAME ENDS AND THE IMPOSTERS WINS. IF THEY NAME A WRONG PLACE, THE GAME CONTINUES WITHOUT THEM.":
+      "მატყუარების რაოდენობა უნდა იყოს ადგილობრივების რაოდენობაზე ნაკლები. თუ ერთზე მეტია, თითოეული მათგანი თავისთვის თამაშობს. იმისათვის რომ მოიგოთ, ადგილობრივებმა უნდა გამოაშკარავონ ყველა მატყუარა. მატყუარები, რომლებიც არ იყვნენ გამოვლენილი, იმარჯვებენ. როდესაც მოთამაშეები მიუთითებენ ადამიანზე, რომელიც, სავარაუდოდ, მატყუარაა: თუ ის ადგილობრივია - მატყუარები იმარჯვებენ. თუ ის მატყუარაა - ის გტოვებთ და თამაში გრძელდება. როდესაც ერთ-ერთი მატყუარა ასახელებს ადგილს: თუ ისინი მართლები არიან - თამაში მთავრდება და მატყუარა იგებს. თუ ისინი არასწორ ადგილს ასახელებენ, თამაში გრძელდება მათ გარეშე.",
+    "I THINK THAT CARDS ARE NOT RANDOM.":
+      "ვფიქრობ, რომ ბარათები არ არის შემთხვევით განაწილებული",
+    "DON'T WORRY, WE HAVE CHECKED. THE CARDS ARE RANDOM.":
+      "არ ინერვიულოთ, ჩვენ გადავამოწმეთ. ბარათები შემთხვევითია.",
   },
 };
